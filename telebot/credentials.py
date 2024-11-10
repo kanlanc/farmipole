22195717510552463971837074890419cf72f44e
Vikranth Kanumuru <saivicky2015@gmail.com>
2024-11-10 09:28:06 -0800

    added the url cred

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token ="7826918701:AAGmBIL9xOrHN6JlQDJHQdBqYkkL2r9KSqI"
 bot_user_name = "@Farmipolebot"
-URL = "the heroku app link that we will create later"
+URL = "https://farmipole-69ae5776284b.herokuapp.com/"
