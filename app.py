61fdc883065fdfc5ae96cf09e38b29de32f7cb15
Vikranth Kanumuru <saivicky2015@gmail.com>
2024-11-10 10:05:10 -0800

    redid the entire thing

--- app.py
+++ app.py
@@ -1,66 +1,41 @@
-import re
-from flask import Flask, request
-import telegram
-from telebot.credentials import bot_token, bot_user_name,URL
+from flask import Flask, request, jsonify
+import telebot
+from telebot.handler_backends import WebhookHandler
 
-
-global bot
-global TOKEN
-TOKEN = bot_token
-bot = telegram.Bot(token=TOKEN)
-
+# Initialize Flask and bot
 app = Flask(__name__)
+bot = telebot.TeleBot("7826918701:AAGmBIL9xOrHN6JlQDJHQdBqYkkL2r9KSqI", threaded=False)
 
-@app.route('/{}'.format(TOKEN), methods=['POST'])
-def respond():
-   # retrieve the message in JSON and then transform it to Telegram object
-   update = telegram.Update.de_json(request.get_json(force=True), bot)
+# Webhook configuration
+WEBHOOK_URL = 'farmipole-69ae5776284b.herokuapp.com'  # Replace with your domain
+WEBHOOK_PATH = f'/webhook/{bot.token}'
 
-   chat_id = update.message.chat.id
-   msg_id = update.message.message_id
+# Bot handlers
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
 
-   # Telegram understands UTF-8, so encode text for unicode compatibility
-   text = update.message.text.encode('utf-8').decode()
-   # for debugging purposes only
-   print("got text message :", text)
-   # the first time you chat with the bot AKA the welcoming message
-   if text == "/start":
-       # print the welcoming message
-       bot_welcome = """
-       Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
-       """
-       # send the welcoming message
-       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
+# Flask routes
+@app.route(WEBHOOK_PATH, methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return jsonify({'ok': True})
+    else:
+        return jsonify({'ok': False})
 
-   else:
-       try:
-           # clear the message we got from any non alphabets
-           text = re.sub(r"\W", "_", text)
-           # create the api link for the avatar based on http://avatars.adorable.io/
-           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-           # reply with a photo to the name the user sent,
-           # note that you can send photos by url and telegram will fetch it for you
-           bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-       except Exception:
-           # if things went wrong
-           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
-
-   return 'ok'
-
-@app.route('/setwebhook', methods=['GET', 'POST'])
+@app.route('/set_webhook')
 def set_webhook():
-   s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-   if s:
-       print(s)
-       return "webhook setup ok"
-   else:
-       return "webhook setup failed"
-
-@app.route('/')
-def index():
-   return '.'
-
+    webhook_url = f'{WEBHOOK_URL}{WEBHOOK_PATH}'
+    bot.remove_webhook()
+    bot.set_webhook(url=webhook_url)
+    return f"Webhook set to {webhook_url}"
 
 if __name__ == '__main__':
-   app.run(threaded=True)
+    app.run(host='0.0.0.0', port=5000)
